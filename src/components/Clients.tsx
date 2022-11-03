d9774d1044f72ed9f1f4bbc6262a6f30920a4d2f
christian.benitez <christian.p.benitez@gmail.com>
2022-11-03 16:10:57 -0700

    Add footer.

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -4,7 +4,7 @@ import styles from '../style'
 const Clients = () => {
   return (
     <section className={`${styles.flexCenter} my-4`}>
-      <div className={`${styles.flexCenter} w-full`}>
+      <div className={`${styles.flexCenter} w-full flex-wrap`}>
         {clients.map(client => (
           <div key={client.id} 
             className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
